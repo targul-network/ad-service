7309b8ad3b575db0d0b1eb11614b0d1b34f4ae6a
SerhiiRybak <sergey24rybak@gmail.com>
2024-07-23 02:34:35 +0300

    Initial commit

---
+++ src/test/java/net/targul/adservice/AdServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package net.targul.adservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

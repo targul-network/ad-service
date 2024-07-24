daa9b4cebb805df246ba509a640537f0eb50cf5d
SerhiiRybak <sergey24rybak@gmail.com>
2024-07-24 23:01:36 +0300

    Logger features were implemented. Logging features were added to AdEventProducer. Server port was changed to 8001

--- src/main/java/net/targul/adservice/kafka/producer/AdEventProducer.java
+++ src/main/java/net/targul/adservice/kafka/producer/AdEventProducer.java
@@ -1,11 +1,14 @@
 package net.targul.adservice.kafka.producer;
 
 import net.targul.adservice.kafka.event.AdCreatedEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AdEventProducer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(AdEventProducer.class);
     private static final String TOPIC = "ad-created-events";
 
     private final KafkaTemplate<String, AdCreatedEvent> kafkaTemplate;
@@ -15,6 +18,7 @@ public class AdEventProducer {
     }
 
     public void sendAdCreatedEvent(AdCreatedEvent adCreatedEvent) {
+        LOGGER.info("EVENT HAS BEEN SENT!!!!!!!!!!!!!!");
         kafkaTemplate.send(TOPIC, adCreatedEvent);
     }
 }
